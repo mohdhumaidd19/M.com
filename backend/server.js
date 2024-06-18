4dc88faba86e74b569f88645ca0bfce46a14821c
mohdhumaidd19 <mohdhumaidd@gmail.com>
2024-06-18 14:52:31 +0530

    18 june

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import path from "path";
 import express from "express";
 import authRuotes from "./routes/authroutes.js"
 import userRoutes from "./routes/userRoutes.js"
@@ -7,6 +8,7 @@ import dotenv from "dotenv";
 import { v2 as cloudinary } from "cloudinary";
 import connectMongoDb from "./db/connectMongoDb.js";
 import cookieParser from "cookie-parser";
+import cors from "cors"
 dotenv.config();
 cloudinary.config({
     cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
@@ -15,6 +17,15 @@ cloudinary.config({
 });
 const app = express();
 const PORT = process.env.PORT || 5000;
+app.use(cors(
+   {
+    origin:[""],
+    methods:["POST","GET"],
+    credentials:true
+   } 
+    
+));
+const __dirname=path.resolve()
 app.use(express.json({limit:"5mb"}));//to parse req.body
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
@@ -22,6 +33,15 @@ app.use("/api/auth", authRuotes);
 app.use("/api/users", userRoutes);
 app.use("/api/post", postroutes);
 app.use("/api/notifications",notificationroutes);
+
+if (process.env.NODE_ENV === "production") {
+	app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+	app.get("*", (req, res) => {
+		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+	});
+}
+
 app.listen(PORT, () => {
     console.log(`server is running on port ${PORT}`);
     connectMongoDb();
